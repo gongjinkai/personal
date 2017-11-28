04d26d882cd4faa8e3ba311b57ed4d8dc88a3cb3
author <498752819@qq.com>
2017-11-28 09:18:33 +0800

    'commonjs'

---
+++ CommonJS/add.js
@@ -0,0 +1,6 @@
+﻿var example = require('./module/example');
+var jquery = require('./module/jquery-2.1.1.min');
+exports.$ = jquery;
+console.log(module);
+console.log(example.x);
+console.log(example.addX);
---
+++ CommonJS/module/example.js
@@ -0,0 +1,7 @@
+﻿var x = 5;
+var addX = function(value){
+    return value + x;
+}
+
+module.exports.x = x;
+module.exports.addX = addX;
