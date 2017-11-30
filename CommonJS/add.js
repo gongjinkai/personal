3cc792518bff867c26da3a8dd339290fb580e4aa
author <498752819@qq.com>
2017-11-30 22:03:09 +0800

    'cm'

--- CommonJS/add.js
+++ CommonJS/add.js
@@ -1,6 +1,10 @@
 ﻿var example = require('./module/example');
-var jquery = require('./module/jquery-2.1.1.min');
-exports.$ = jquery;
+var a = require('./module/emitter');
+var example1 = require('./module/example1');
+example1
+a.on('ready', function() {
+    console.log('module a is ready');
+});
 console.log(module);
 console.log(example.x);
 console.log(example.addX);
---
+++ CommonJS/module/example1.js
@@ -0,0 +1,10 @@
+﻿var invisible = function () {
+    console.log("invisible");
+}
+
+exports.message = "hi";
+
+exports.say = function () {
+    console.log(message);
+}
+
