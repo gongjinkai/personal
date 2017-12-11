839fd9d1e6f3de4ad47885e472dd58d51171ceb4
author <498752819@qq.com>
2017-12-11 23:45:27 +0800

    'parseurl'

---
+++ 获取地址栏参数.js
@@ -0,0 +1,14 @@
+﻿ function parseUrl(url){
+        var obj = {};
+        var keyvalue = [];
+        var key = "";
+        var value = "";
+        var parseString = url.substring(url.indexOf("?")+1,url.length).split("&");
+        for(var i in parseString){
+            keyvalue = parseString[i].split("=");
+            key = keyvalue[0];
+            value = keyvalue[1];
+            obj[key] = value
+        }
+        return obj;
+    }
